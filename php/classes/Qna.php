c4e52b060e040a64cd188b9b2f9045c53aa1e625
Monica Alvarez <mmalvar13@gmail.com>
2016-10-01 15:17:00 -0600

    mutator methods for qna class https://app.asana.com/0/186925364784027/186925364784059

--- php/classes/Qna.php
+++ php/classes/Qna.php
@@ -121,6 +121,112 @@ class Qna implements \JsonSerializable {
 	}
 
 
+	/*--------------------------------------------Mutator Methods--------------------------------------------------*/
+
+	/**
+	 * mutator method for qnaId
+	 * @param int|null $newQnaId new value of qnaId
+	 * @throws \InvalidArgumentException if data types are not valid
+	 * @throws \RangeException if $newQnaId is not positive
+	 * @throws \TypeError if$newQnaId is not an integer
+	 **/
+	public function setQnaId(int $newQnaId = null) {
+		//base case: if the qnaId is null, this is a new qna without a mySQL assigned id yet
+		if($newQnaId === null) {
+			$this->qnaId = null;
+			return;
+		}
+		//verify that the qnaId is positive
+		if($newQnaId <= 0) {
+			throw(new \RangeException("qna id is not positive"));
+		}
+
+		//convert and store the qna id
+		$this->qnaId = $newQnaId;
+	}
+
+
+	/**
+	 * mutator method for qnaCategoryId
+	 * @param int $newQnaCategoryId new value of qnaCategoryId
+	 * @throws \RangeException if $newQnaCategoryId is not positive
+	 * @throws \TypeError if $newQnaCategoryId is not an integer
+	 **/
+	public function setQnaCategoryId(int $newQnaCategoryId) {
+		//verify that the $newQnaCategoryId is positive
+		if($newQnaCategoryId <= 0) {
+			throw(new \RangeException("the qna category id is not positive"));
+		}
+
+		//convert and store the qna category id
+		$this->qnaCategoryId = $newQnaCategoryId;
+	}
+
+
+	/**
+	 * mutator method for qnaAnswer
+	 * @param string $newQnaAnswer new value of qnaAnswer
+	 * @throws \RangeException if $newQnaAnswer is empty or too long
+	 * @throws \InvalidArgumentException if $newQnaAnswer is not a string
+	 * @throws \TypeError if $newQnaAnswer is not a string
+	 **/
+	public function setQnaAnswer(string $newQnaAnswer) {
+		//strip out white space on either end of $newQnaAnswer
+		$newQnaAnswer = trim($newQnaAnswer);
+		//sanitize $newQnaAnswer
+		$newQnaAnswer = filter_var($newQnaAnswer, FILTER_SANITIZE_STRING);
+		//check if $newQnaAnswer is empty
+		if(strlen($newQnaAnswer) === 0) {
+			throw(new \RangeException("Must enter Qna Answer"));
+		}
+		//check if $newQnaAnswer is too long
+		if(strlen($newQnaAnswer) > 256) {
+			throw(new \RangeException("Qna Answer is too long"));
+		}
+		//convert and store the qna answer
+		$this->qnaAnswer = $newQnaAnswer;
+	}
+
+
+	/**
+	 * mutator method for qnaPointVal
+	 * @param int $newQnaPointVal is the new value of qnaPointVal
+	 * @throws
+	 **/
+
+
+	/**
+	 * mutator method for qnaQuestion
+	 * @param string $newQnaQuestion is the new value of qnaQuestion
+	 * @throws \RangeException if $newQnaQuestion is empty or too long
+	 * @throws \InvalidArgumentException if $newQnaQuestion is not a string
+	 * @throws \TypeError if $newQnaQuestion is not a string
+	 **/
+	public function setQnaQuestion(string $newQnaQuestion) {
+		//strip out white space on either end of qnaQuestion
+		$newQnaQuestion = trim($newQnaQuestion);
+
+		//sanitize $newQnaQuestion
+		$newQnaQuestion = filter_var($newQnaQuestion, FILTER_SANITIZE_STRING);
+
+		//check if $newQnaQuestion is empty
+		if(strlen($newQnaQuestion) === 0){
+			throw(new \RangeException("Must enter a QNA question"));
+		}
+
+		//check if $newQnaQuestion is too long
+		if(strlen($newQnaQuestion) > 256){
+			throw(new \RangeException("qnaQuestion is too long"));
+		}
+
+		//convert and store the qnaQuestion
+		$this->qnaQuestion = $newQnaQuestion;
+	}
+
+
+	/*----------------------------------------------Get Foo By Bars--------------------------------------------*/
+
+
 
 
 
