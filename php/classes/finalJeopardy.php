610f3991886ad6968624a50f7d2e1d342b45a2f2
Giles Sandoval <gsandoval2277@gmail.com>
2016-10-01 18:05:25 -0600

    comments for the project added

--- php/classes/finalJeopardy.php
+++ php/classes/finalJeopardy.php
@@ -3,3 +3,9 @@ namespace Edu\Cnm\DDCBJeopardy;
 
 require_once ("autoloader.php");
 
+/**
+ * Class for Final Jeopardy round of the game - queue the music
+ *
+ * @author Giles Sandoval <gsandoval2277@gmail.com>
+ * @version 1.0.0
+ */
