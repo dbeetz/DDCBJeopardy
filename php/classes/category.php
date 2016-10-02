a25424f835a325b0a0531b0fce63356dcc6b3667
Christina Sosa <csosa2106@gmail.com>
2016-10-01 18:28:44 -0600

    created constructor for category

--- php/classes/category.php
+++ php/classes/category.php
@@ -6,3 +6,39 @@ require_once ("autoloader.php");
  * @author Christina Sosa <csosa4@cnm.edu>
  *
  **/
+
+class Category implements \JsonSerializable{
+
+	/**
+	 * Id for category; this is the primary key
+	 * @var int $categoryId
+	**/
+	private $categoryId;
+
+	/**
+	 * this is the foreign key from Game
+	 * @var int $categoryGameId
+	**/
+	private $categoryGameId;
+
+	/**
+	 * name of category
+	 * @var string $categoryName
+	**/
+	private $categoryName;
+
+	/**
+	 * Constructor for this Category
+	 *
+	 * @param int|null $newCategoryId of this category or null if a new category
+	 *@param int|null $newCategoryGameId id for the key from Game
+	 *@param string $newCategoryName string containing category name
+	 *@throws \InvalidArgumentException if data types are not valid
+	 *@throws \RangeException if data values are out of bounds
+	 *@throws \TypeError if data types violate type hints
+	 *@throws \Exception if some other exception occurs
+	**/
+
+	public function __construct() {
+	}
+}
--- test/categoryTest.php
+++ test/categoryTest.php
@@ -1,7 +1,1 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Sosa
- * Date: 10/1/16
- * Time: 5:32 PM
- */
