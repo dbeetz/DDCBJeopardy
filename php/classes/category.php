2371c2e92d0494dfe11b587c5efb8ef7a9f3e793
Christina Sosa <csosa2106@gmail.com>
2016-10-01 18:14:55 -0600

    created category class and test files

---
+++ php/classes/category.php
@@ -0,0 +1,8 @@
+<?php
+namespace Edu\Cnm\DDCBJeopardy;
+require_once ("autoloader.php");
+
+/**
+ * @author Christina Sosa <csosa4@cnm.edu>
+ *
+ **/
---
+++ test/categoryTest.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Sosa
+ * Date: 10/1/16
+ * Time: 5:32 PM
+ */
